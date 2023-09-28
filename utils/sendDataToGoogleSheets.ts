40bb3b2b4a737a0046b73d249ee98963ca5e1f79
unknown <tana.palam@gmail.com>
2023-09-28 18:40:15 +0300

    fix send data

--- utils/sendDataToGoogleSheets.ts
+++ utils/sendDataToGoogleSheets.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { CareerFormFields } from "@/types";
-const GOOGLE_SHEETS_SCRIPT_URL =
-  process.env.NEXT_PUBLIC_GOOGLE_SHEETS_SCRIPT_URL;
+const GOOGLE_SHEETS_SCRIPT_URL = process.env
+  .NEXT_PUBLIC_GOOGLE_SHEETS_SCRIPT_URL as string;
 
 export const sendDataToGoogleSheets = async (data: CareerFormFields) => {
   try {
-    await axios.post(`${GOOGLE_SHEETS_SCRIPT_URL}`, data);
+    await axios.post(GOOGLE_SHEETS_SCRIPT_URL, data);
   } catch (error) {
     throw new Error();
   }
